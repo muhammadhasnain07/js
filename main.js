307edf16cb3f4f217a9ff9f6dc5108ca67ee9b5a
unknown <hmhsabbasiaptech07@gmail.com>
2025-08-06 22:49:51 -0700

    7/aug/25

---
+++ main.js
@@ -0,0 +1,10 @@
+//arithmatic oprator
+
+let num1=prompt("enter first number")
+let num2 = prompt("enter second number")
+// let result = num1 + num2
+let result = num1 - num2
+// let result = num1 * num2
+// let result = num1 / num2
+// let result = num1 % num2
+document.getElementById("para").innerHTML=result;
