360518f7724e79663939b2c17d4cae9f964838d7
TaimoorQadri123 <dchota374@gmail.com>
2025-08-29 22:37:19 -0700

    29/aug/25

---
+++ loop/loop/app.js
@@ -0,0 +1,34 @@
+// // for loop
+
+
+// for(let i = 1; i<= 10; i++){
+//     document.write(i,":this is for loop <br>");
+// }
+
+
+// for(let i = 10; i>= 1; i--){
+//     document.write(i,":this is for loop <br>");
+// }
+
+
+
+
+// let table = prompt("enter a table number")
+// for (let i = 1; i <=10; i++){
+//     document.write(table , "x",i, "=", table * i, "<br>")
+// }
+
+
+// while loop
+
+// let a = 10;
+// while(a<=100000){
+//     document.write(a, "this is while loop <br>")
+//     a++
+// } 
+
+let a = 1000;
+while(a>=1){
+    document.write(a, "this is while loop <br>")
+    a--
+} 
---
+++ switch.js
@@ -0,0 +1,45 @@
+// let languageName = prompt("Enter Your Favorite Language");
+// switch(languageName){
+//     case "nahw":
+//         document.write("ase usoolon ka ilm jin k zariye hum kalimy k ahwaal ko tabdeel karty hain");
+//         break;
+//         case "sarf":
+//             document.write("ase usoolon ks ilm jin k zariye hum ek kalimy se dusra kalima banaty hai");
+//                 break;
+//             case "fiq":
+//                 document.write("hum is me awami masail sikhty hain");
+//                     break;
+//                 case "usool e fiq":
+//                     document.write("hum is me quran o hadees se masly nikalna sikhty hain");
+//                         break;
+//                     default:
+//                         document.write("please enter your currect language");
+//                         break;
+// }
+
+
+
+
+
+
+let wovelName = prompt("Enter Your wovel number");
+switch(wovelName){
+    case "a":
+        document.write("wovel number");
+        break;
+        case "e":
+            document.write("wovel number");
+                break;
+            case "i":
+                document.write("wovel number");
+                    break;
+                case "o":
+                    document.write("wovel number");
+                        break;
+                        case "u":
+                    document.write("wovel number");
+                        break;
+                    default:
+                        document.write("please enter your currect language");
+                        // break;
+}
